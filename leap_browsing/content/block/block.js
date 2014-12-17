8c786d217810872ed8ae7dd67d78b909cf45453e
naoe <b111081@cc.it-hiroshima.ac.jp>
2014-12-18 05:25:25 +0900

    システムい機能を実�
    ・ブックマーク追加機能
    ・タブ削除機能

--- leap_browsing/content/block/block.js
+++ leap_browsing/content/block/block.js
@@ -1,108 +1,103 @@
 // ページをブロック要素に変換する関数
-var Block = {
-  startBlock: function(opt_uri) {
-    // 新規ページのインスタンス作成
-      var newTab = gBrowser.getBrowserForTab(gBrowser.addTab(opt_uri));
+var Block = function(opt_uri) {
+  // 新規ページのインスタンス作成
+    var newTab = gBrowser.getBrowserForTab(gBrowser.addTab(opt_uri));
 
-    // 開いたタブにフォーカスを移す
-    gBrowser.tabContainer.advanceSelectedTab(1, true);
+  // 開いたタブにフォーカスを移す
+  gBrowser.selectedTab = gBrowser.tabContainer.childNodes[gBrowser.tabContainer.childNodes.length-1];
 
-    // ページを読み込み終わると次の処理に移る
-    newTab.addEventListener("load", function() {
-      // ページのhtmlをJqueryのDOM要素に変換し変数に格納
-      var $dom = $(newTab.contentDocument);
+  // ページを読み込み終わると次の処理に移る
+  newTab.addEventListener("load", function() {
+    // ページのhtmlをJqueryのDOM要素に変換し変数に格納
+    var $dom = $(newTab.contentDocument);
 
-      // 不要な要素を削除
-      //$dom.find("meta").remove();
-      $dom.find("link").remove();
+    // 不要な要素を削除
+    //$dom.find("meta").remove();
+    $dom.find("link").remove();
 
-      // 出力用ノード配列
-      var node = [];
-      // ブロック要素のhtmlを格納する配列
-      var block_html = [];
+    // 出力用ノード配列
+    var node = [];
+    // ブロック要素のhtmlを格納する配列
+    var block_html = [];
 
-      // 全要素数を取得.
-      var len = $dom.find("*").length;
-      // 深さ用変数
-      var deps = 0;
-      // ログ出力用変数
-      var str;
+    // 全要素数を取得.
+    var len = $dom.find("*").length;
+    // 深さ用変数
+    var deps = 0;
+    // ログ出力用変数
+    var str;
 
-      // DOMループ処理
-      for (var i = 0; i < len; i++){
-        // コンソールに全要素を表示
-        str = deps + "__" + $dom.get(0).tagName;
-        node.push(str);
-        //console.log(str);
+    // DOMループ処理
+    for (var i = 0; i < len; i++){
+      // コンソールに全要素を表示
+      str = deps + "__" + $dom.get(0).tagName;
+      node.push(str);
+      //console.log(str);
 
-        // 特定要素をブロック要素に変換し、配列に格納
-        if ($dom.get(0).tagName == "P" || $dom.get(0).tagName == "A" || $dom.get(0).tagName == "H2" || $dom.get(0).tagName == "PRE" || $dom.get(0).tagName == "SPAN"){
-          var temp_text = $dom.text().replace(/\"/g, 'だぶるくおーてーしょん');
-          if ($dom.get(0).tagName == "A"){
-            block_html[i] = 
-            '<div id = "block" style="float:left;"><button id="block_text" aria-label="' + temp_text + '" style="width : 200px;height : 200px; word-break:keep-all;"><a href="' + $dom.get(0).href + '">' + temp_text + '</ button></ div>';
-          } else {
-            block_html[i] = '<div id = "block" style="float:left;"><button id="block_text" aria-label="' + temp_text +'" style="width : 200px;height : 200px; word-break:keep-all;">' + temp_text + '</button></div>';
-          }
+      // 特定要素をブロック要素に変換し、配列に格納
+      if ($dom.get(0).tagName == "P" || $dom.get(0).tagName == "A" || $dom.get(0).tagName == "H2" || $dom.get(0).tagName == "PRE" || $dom.get(0).tagName == "SPAN"){
+        var temp_text = $dom.text().replace(/\"/g, 'だぶるくおーてーしょん');
+        if ($dom.get(0).tagName == "A"){
+          block_html[i] = 
+          '<div id = "block" style="float:left;"><button id="block_text" aria-label="' + temp_text + '" style="width : 200px;height : 200px; word-break:keep-all;"><a href="' + $dom.get(0).href + '">' + temp_text + '</ button></ div>';
+        } else {
+          block_html[i] = '<div id = "block" style="float:left;"><button id="block_text" aria-label="' + temp_text +'" style="width : 200px;height : 200px; word-break:keep-all;">' + temp_text + '</button></div>';
         }
-
-        // 子孫が存在する場合
-        if ($dom.children()[0]){
-          // 階層インクリメント
-          deps++;
-          // カレントを更新して子孫に移動する
-          $dom = $dom.children().eq(0);
-          continue;
-        }
-
-        // 子孫がいないので兄弟を探索する
-        if ($dom.next()[0]){
-          $dom = $dom.next();
-          continue;
-        }
-
-        // 子孫も兄弟もいないので親に戻って一つ進む
-        $parent = $dom.parent();
-        deps--;
-
-        do{
-          // 親の兄弟がいる場合はそちらへ
-          if($parent.next().get(0)){
-            $dom = $parent.next();
-            break;
-
-          // 兄弟がいない場合はさらに祖先へ
-          }else{
-            $parent = $parent.parent();
-            deps--;
-          }
-        }while(deps);
       }
 
-      // ブロック配置用のdiv要素でページを上書き
-      newTab.contentDocument.body.innerHTML = "<div id='block_pos'> </div>";
+      // 子孫が存在する場合
+      if ($dom.children()[0]){
+        // 階層インクリメント
+        deps++;
+        // カレントを更新して子孫に移動する
+        $dom = $dom.children().eq(0);
+        continue;
+      }
 
-      // 再度ページのhtmlをDOM要素として変数に格納
-      var $block = $(newTab.contentDocument);
-      
-      // headにポインタに使うCSSを配置
-      var pointer_css = '<style type="text/css">#pointer {width: 50px;height: 50px;-webkit-border-radius: 25px;-moz-border-radius: 25px;border-radius: 25px;background-color: #999;position: absolute;}</style>';
-      $block.find("head").append(pointer_css);
-      
-      // ポインタ要素を配置
-      $block.find("body").append('<div id="pointer"></div>');
-      
-      // ページにブロックを配置
-      for (var i=0; i < len; i++) {
-        $block.find("#block_pos").append(block_html[i]);
-        //console.log(block_html[i]);
-      };
+      // 子孫がいないので兄弟を探索する
+      if ($dom.next()[0]){
+        $dom = $dom.next();
+        continue;
+      }
 
-      // リンクがあるブロックの処理
-      $block.find("#block_pos #block_link").click(function () {
-        uri = $(this).children().get(0).href;
-        Block.startBlock(uri);
-      });
-    }, true);
-  }
+      // 子孫も兄弟もいないので親に戻って一つ進む
+      $parent = $dom.parent();
+      deps--;
+
+      do{
+        // 親の兄弟がいる場合はそちらへ
+        if($parent.next().get(0)){
+          $dom = $parent.next();
+          break;
+
+        // 兄弟がいない場合はさらに祖先へ
+        }else{
+          $parent = $parent.parent();
+          deps--;
+        }
+      }while(deps);
+    }
+
+    // ブロック配置用のdiv要素でページを上書き
+    newTab.contentDocument.body.innerHTML = "<div id='block_pos'> </div>";
+
+    // 再度ページのhtmlをDOM要素として変数に格納
+    var $block = $(newTab.contentDocument);
+      
+    // headにポインタに使うCSSを配置
+    var pointer_css = '<style type="text/css">#pointer {width: 50px;height: 50px;-webkit-border-radius: 25px;-moz-border-radius: 25px;border-radius: 25px;background-color: #999;position: absolute;}</style>';
+    $block.find("head").append(pointer_css);
+     
+    // ポインタ要素を配置
+    $block.find("body").append('<div id="pointer"></div>');
+    
+    // スクロールに使用する要素とスクリプト配置
+    $block.find("body").append('<div id="scroll_down" onclick="window.scrollTo(0,1000);"></div>');
+    
+    // ページにブロックを配置
+    for (var i=0; i < len; i++) {
+      $block.find("#block_pos").append(block_html[i]);
+      //console.log(block_html[i]);
+    };
+  }, true);
 };
--- leap_browsing/content/view/view.js
+++ leap_browsing/content/view/view.js
@@ -10,50 +10,43 @@ var gesture_type;
 var gesture_state;
 // ジェスチャーカウント用変数
 var right_cir_cnt = 0, left_cir_cnt = 0;
-// ジェスチャーの向き用変数
-var clockwiseness;
-// 指の本数用変数
-var finger_length = 0;
 // ブロックの接触判定用変数
 var num = 0, hide_block = 0;
+// ビュー状態用変数
+var view_state = 0;
+// ビュー切り替えの時間差に用いる変数
+var active_time = 0;
+// ビューのフラグ管理用の変数
+var view_flag;
 
-// LeapMotion処理のループ関数
+// メイン処理のループ
 Leap.loop({enableGestures: true}, function(frame){
   // ポインターの更新処理
-  if (frame.fingers[0]) {
-    updatePointer(frame);
-  }
+  updatePointer(frame);
   
-  // 描いた円の処理
+  // 円を描くと現在のタブを削除
   getCirclegesture(frame);
-  
-  // 開いている指の本数を取得
-  var finger_length = getFinger(frame);
+ 
+  // 指を一定時間開いていればフラグ成立
+  activeFinger(getFinger(frame)); // クロージャでつくるべき
 
-  // タブビューを開く処理
-  if (finger_length == 2 && right_cir_cnt == 1 && sw_1 == 0 && sw_3 == 0) { openTabview(); }
-  if (sw_1 == 1) { selectTabview(); }
-  if (sw_2 == 1) { initTabview(); }
+  // 一定時間2本指でタブビューを開く
+  if (view_flag == 1 && sw_1 == 0 && sw_3 == 0) { openTabview(); }
 
-  // ブックマークビューを開く処理
-  if (finger_length == 3 && left_cir_cnt == 1 && sw_3 == 0 && sw_1 == 0) { openBkview(); }
-  if (sw_3 == 1 && sw_4 == 0) { selectBkview(); }
-  if (sw_4 == 1) { initBkview(); }
-
-  gesture_type = 0;
-  finger_length = 0;
+  // 一定時間3本指でブックマークビューを開く
+  if (view_flag == 2 && sw_3 == 0 && sw_1 == 0) { openBkview(); }
 });
 
 // 指の本数取得用関数
-function getFinger(opt_frame) {
+var getFinger =  function(opt_frame) {
   // 手が認識されているか
   if (opt_frame.hands.length > 0) {
-    //受け取ったframeオブジェクトから指に関する値を取得
+    // 受け取ったframeオブジェクトから指に関する値を取得
     var hand = opt_frame.hands[0];
     var finger = hand.fingers[0];
     var extendedFingers = 0;
-		
-    //開いている指の本数を取得
+
+    // 開いている指の本数を取得
     for(var f = 0; f < hand.fingers.length; f++){
       var finger = hand.fingers[f];
       if(finger.extended) extendedFingers++;
@@ -62,55 +55,124 @@ function getFinger(opt_frame) {
     extendedFingers = 0;
   }
   return extendedFingers;
-}
+};
+
+// ビューのフラグ管理用関数
+var activeFinger =  function(opt_finger) {
+  // 2本or3本指を一定時間でフラグが成立
+  if (opt_finger == 2) {
+  	active_time ++;
+  	if (active_time > 80) {
+  	  view_flag = 1;
+  	  active_time = 0;
+    }
+  } else if (opt_finger == 3) {
+  	active_time ++;
+  	if (active_time > 80) {
+  	  view_flag = 2;
+  	  active_time = 0;
+    }
+  } else {
+  	// 2本or3本指以外だとタイマー初期化
+  	active_time = 0;
+  }
+};
 
 // ポインタ座標更新用関数
-function updatePointer(opt_frame) {
-  // 現在フォーカスしているタブのDOMを取得
-  var $forcus_tab = $(gBrowser.contentDocument);
-  // Leapの座標を画面と対応させる
-  var pointer_x = (opt_frame.fingers[0].tipPosition[0] * 5) + (window.parent.screen.width / 2);
-  var pointer_y = (window.parent.screen.height  / 1.5) - (opt_frame.fingers[0].tipPosition[1] * 3);
-  // 座標更新のためのオブジェクト作成
-  var position = new Object();
-  position.left = pointer_x;
-  position.top = pointer_y;
-  
-  // ポインタの座標を更新
-  if ($forcus_tab.find("#pointer").length != 0) {
-  	$forcus_tab.find("#pointer").offset(position);
+var updatePointer = function(opt_frame) {
+  if (opt_frame.fingers[0]) {
+    // 現在フォーカスしているタブのDOMを取得
+    var $forcus_tab = $(gBrowser.contentDocument);
+    // Leapの座標を画面と対応させる
+    var pointer_x = (opt_frame.fingers[0].tipPosition[0] * 5) + (window.parent.screen.width / 2);
+    var pointer_y = (window.parent.screen.height  / 1.5) - (opt_frame.fingers[0].tipPosition[1] * 3);
+    // 座標更新のためのオブジェクト作成
+    var position = new Object();
+    position.left = pointer_x;
+    position.top = pointer_y;
+
+    // ポインタの座標を更新
+    if ($forcus_tab.find("#pointer").length != 0) {
+  	  $forcus_tab.find("#pointer").offset(position);
+    }
+
+    // ブロックと接触しているか判定
+    collisionBlock($forcus_tab, opt_frame, pointer_y);
   }
-  
-  // ブロックと接触しているか判定
-  collisionBlock($forcus_tab);
-}
+};
 
 // ポインタとブロックの接触判定処理用関数
-function collisionBlock($opt_tab) {
-  if ($opt_tab.find("#block_pos #block").length > num) {
+var collisionBlock =  function($opt_tab, opt_frame, opt_pointer_y) {
+  // 調査ループに使用する変数
+  var block_loop = $opt_tab.find("#block_pos #block").length;
+
+  // ブロックの数だけ接触しているか調査
+  if (block_loop > num) {
   	var b = $opt_tab.find("#block_pos #block").eq(num);
   	var block_col = $opt_tab.find("#pointer").collision(b);
-  	if (block_col.length != 0) {
-  	  $opt_tab.find("#block_pos #block_text").eq(num).focus();
-  	  console.log($opt_tab.find("#block_pos #block_text").eq(num).val());
-  	} else {
-  		num++;
-  	}
-  } else {
-  	num = 0;
-  }
-  /*$opt_tab.find("#block_pos #block_text").each(function(index) {
-    //var b = $opt_tab.find("#block_pos #block_text").eq(num);
-    var block_col = $opt_tab.find("#pointer").collision($(this));
-    //console.log(block_col);
+
+  	// 接触していた場合ブロックにフォーカスする
     if (block_col.length != 0) {
-  	  $(this).focus();
-  	}
-  });*/
-}
+  	  $opt_tab.find("#block_pos #block_text").eq(num).focus();
+
+  	  // スクロール処理
+  	  scrollBrowser($opt_tab, opt_pointer_y, num);
+
+  	  // 調査するブロックの範囲を絞る
+      block_loop = num +5;
+
+  	  // タップジェスチャーをした場合ブロックを押す
+  	  if (typeof(opt_frame.gestures) != 'undefined' ) {
+        for (var i=0; i < opt_frame.gestures.length; i++) {
+          gesture_type = opt_frame.gestures[i].type;
+  	      if (gesture_type == 'keyTap' ) {
+  	        // ビューの状態により処理を分ける
+  	      	switch (view_state) {
+  	      	  case 0:
+  	      	    // 音声を再生する
+   	            $opt_tab.find("body").append('<audio src="http://localhost/audio/cancel.mp3" autoplay></audio>');
+                $opt_tab.find("audio").get(0).play(); 
+  	      	    var uri = $opt_tab.find("#block_pos #block_text").eq(num).children().get(0).href;
+                Block(uri);
+                break;
+  	      	  case 1:
+  	      	    selectTabview(num);
+  	      	    break;
+  	      	  case 2:
+  	      	    selectBkview(num);
+  	      	    break;
+  	      	}
+  	      }
+  	    }
+  	  }
+  	  } else { num++; }
+    } else { num = 0; }
+};
+
+// 画面スクロール処理用関数
+var scrollBrowser =  function($opt_tab, opt_pointer_y, opt_num) {
+  // 下にスクロールする場合の底
+  var bottom = window.parent.screen.height * (4/5);
+  // 上にスクロールする場合の天井
+  var top = window.parent.screen.height / 4;
+  // ポインタのY座標を調整
+  var y  = Math.floor(opt_pointer_y * 2);
+  
+  // 下にスクロール
+  if (y > bottom) {
+  	$opt_tab.find("#scroll_down").click();
+  }
+  
+  // 上にスクロール
+  if (opt_pointer_y > top) {
+  }
+};
 
 // 円の情報処理用関数
-function getCirclegesture(opt_frame) {
+var getCirclegesture =  function(opt_frame) {
+  // ジェスチャーの向き用変数
+  var clockwiseness;
+  
   // 円を描くジェスチャー取得
   if (typeof(opt_frame.gestures) != 'undefined' ) {
     for (var i=0; i < opt_frame.gestures.length; i++) {
@@ -137,10 +199,30 @@ function getCirclegesture(opt_frame) {
       left_cir_cnt++;
     }
   }
-}
+  
+  // 反時計周り2回でタブ削除
+  if (left_cir_cnt == 2) {
+  	gBrowser.removeCurrentTab();
+  	left_cir_cnt = 0;
+  }
+  
+  // 時計回り3回で現在のページをブックマーク
+  if (right_cir_cnt == 3) {
+  	var bmsvc = Components.classes["@mozilla.org/browser/nav-bookmarks-service;1"]
+                      .getService(Components.interfaces.nsINavBookmarksService);
+  	var io = Components.classes["@mozilla.org/network/io-service;1"]
+                    .getService(Components.interfaces.nsIIOService);
+    var uri = io.newURI(gBrowser.contentDocument.location, null, null);
+    var newBkmkId = bmsvc.insertBookmark(bmsvc.toolbarFolder, uri, -1, gBrowser.contentDocument.title);
+  	right_cir_cnt = 0;
+  }
+  
+};
 
 // タブビュー表示用関数
-function openTabview() {
+var openTabview =  function() {
+  // ビュー識別の変数処理
+  view_state = 1;
   // 現在開かれているタブの数を取得
   var num = gBrowser.tabContainer.childNodes.length;
   // タブ情報を格納する配列
@@ -157,20 +239,27 @@ function openTabview() {
   // ページを読み込み終わると次の処理に移る
   tabview.addEventListener("load", function() {
     // ブロック配置用のdiv要素でタブビューを上書き
-    tabview.contentDocument.body.innerHTML = "<div id='tab_block_pos'> </div>";
+    tabview.contentDocument.body.innerHTML = "<div id='block_pos'> </div>";
 
     // タブビューをJqueryのDOM要素に変換し変数に格納
     $tabview_dom = $(tabview.contentDocument);
 
+    // headにポインタに使うCSSを配置
+    var pointer_css = '<style type="text/css">#pointer {width: 50px;height: 50px;-webkit-border-radius: 25px;-moz-border-radius: 25px;border-radius: 25px;background-color: #999;position: absolute;}</style>';
+    $tabview_dom.find("head").append(pointer_css);
+      
+    // ポインタ要素を配置
+    $tabview_dom.find("body").append('<div id="pointer"></div>');
+    
     // タブ情報を配列に格納し、ブロック用配列に格納
     for (var i = 0; i < num; ++i) {
       var r = [];
       r[i] = i+1;
       Tab[i] = gBrowser.tabContainer.childNodes[i];
       Tab_block[i] =
-         '<div id="block" style="float:left;"><button id="tab_block" aria-label="タブ' + r[i] + '" value="' + r[i] +'" style="width : 200px;height : 400px; word-break:keep-all;">' + r[i] + '</button></div>'; 
+         '<div id="block" style="float:left;"><button id="block_text" aria-label="タブ' + r[i] + '" value="' + r[i] +'" style="width : 200px;height : 400px; word-break:keep-all;">' + r[i] + '</button></div>'; 
       //console.log(Tab_block[i]);
-      $tabview_dom.find("#tab_block_pos").append(Tab_block[i]);
+      $tabview_dom.find("#block_pos").append(Tab_block[i]);
     }
 
     // 音声を再生する
@@ -180,36 +269,39 @@ function openTabview() {
 
   // ループ制御
   sw_1 = 1;
-}
+};
 
 // タブ選択用関数
-function selectTabview() {
+var selectTabview =  function(opt_num) {
   $tabview_dom = $(tabview.contentDocument);
-  // タブブロックが押されたか
-  $tabview_dom.find("#tab_block_pos #tab_block").click(function () {
-  	if (sw_2 == 0) {
-  	  // 音声を再生する
-      $tabview_dom.find("audio").remove();
-   	  $tabview_dom.find("body").append('<audio src="http://localhost/audio/cancel.mp3" autoplay></audio>');
-      $tabview_dom.find("audio").get(0).play();
-      // 音声が再生終了したら閉じる処理をする
-      var promise = closeView($(this).val() - 1);
-      promise.done(function() {
-      });
-   }
-    sw_2 = 1;
-   });
-}
+  if (sw_2 == 0) {
+    // 音声を再生する
+    $tabview_dom.find("audio").remove();
+   	$tabview_dom.find("body").append('<audio src="http://localhost/audio/cancel.mp3" autoplay></audio>');
+    $tabview_dom.find("audio").get(0).play();
+    // 音声が再生終了したら閉じる処理をする
+    var promise = closeView(opt_num);
+    promise.done(function() {
+    });
+  }
+  sw_2 = 1;
+};
 
 // タブビュー変数初期化用関数
-function initTabview() {
+var initTabview  = function() {
+  view_state = 0;
   sw_1 = 0;
   sw_2 = 0;
   right_cir_cnt = 0;
-}
+  view_flag = 0;
+  active_time = 0;
+};
 
 // ブックマークビュー表示用関数
-function openBkview() {
+var openBkview =  function() {
+  // ビュー識別の変数処理
+  view_state = 2;
+  
   // ブックマークビュー用の新規タブを開く
   bkview = gBrowser.getBrowserForTab(gBrowser.addTab());
 
@@ -244,7 +336,7 @@ function openBkview() {
     var bk_num = rootNode.childCount;
 
     // 使用するブックマークフォルダにあるブックマーク取得
-    for (var i = 1; i < rootNode.childCount; i++) {
+    for (var i = 0; i < rootNode.childCount; i++) {
         var node = rootNode.getChild(i);
         bk_title[i] = node.title;
         bk_uri[i] = node.uri;
@@ -253,18 +345,25 @@ function openBkview() {
     // 開けたフォルダを閉じる
     rootNode.containerOpen = false;
     // ブロック配置用のdiv要素でタブビューを上書き
-    bkview.contentDocument.body.innerHTML = "<div id='bk_block_pos'> </div>";
+    bkview.contentDocument.body.innerHTML = "<div id='block_pos'> </div>";
     // ブックマークビューをJqueryのDOM要素に変換し変数に格納
     $bkview_dom = $(bkview.contentDocument);
     // ブックマークブロックを格納する配列
     var bk_block = [];
-
+  
+    // headにポインタに使うCSSを配置
+    var pointer_css = '<style type="text/css">#pointer {width: 50px;height: 50px;-webkit-border-radius: 25px;-moz-border-radius: 25px;border-radius: 25px;background-color: #999;position: absolute;}</style>';
+    $bkview_dom.find("head").append(pointer_css);
+      
+    // ポインタ要素を配置
+    $bkview_dom.find("body").append('<div id="pointer"></div>');
+    
     // ブックマーク情報を配列に格納し、ブロック用配列に格納
     for (var i = 1; i < bk_num; ++i) {
         bk_block[i] =  
-          '<div id="block" style="float:left;"><button id="bk_block" aria-label="' + bk_title[i] +'" value="' + bk_uri[i] +'" style="width : 200px;height : 400px; word-break:keep-all;">' + bk_title[i] + '</button></div>'; 
+          '<div id="block" style="float:left;"><button id="block_text" aria-label="' + bk_title[i] +'" value="' + bk_uri[i] +'" style="width : 200px;height : 400px; word-break:keep-all;">' + bk_title[i] + '</button></div>'; 
       //console.log(Tab_block[i]);
-      $bkview_dom.find("#bk_block_pos").append(bk_block[i]);
+      $bkview_dom.find("#block_pos").append(bk_block[i]);
     }
 
     // 音声を再生する
@@ -274,13 +373,12 @@ function openBkview() {
 
   // ループ制御
   sw_3 = 1;
-}
+};
 
 // ブックマーク選択用関数
-function selectBkview() {
+var selectBkview =  function(opt) {
   $bkview_dom = $(bkview.contentDocument);
   // ブックマークブロックが押されたか
-  $bkview_dom.find("#bk_block_pos #bk_block").one('click', function () {
     if (sw_4 == 0) {
       // 音声を再生する
       $bkview_dom.find("audio").remove();
@@ -288,23 +386,25 @@ function selectBkview() {
    	  // 音声再生終了後処理開始
       $bkview_dom.find("audio").get(0).play();
       // 音声が再生終了したら閉じる処理をする
-      var promise = closeView($(this).val());
+      var promise = closeView($bkview_dom.find("#block_pos #block_text").eq(opt).val());
       promise.done(function() {
       });
     }
     sw_4 = 1;
-  });
-}
+};
 
 // ブックマークビュー変数初期化用関数
-function initBkview() {
+var initBkview =  function() {
+  view_state = 0;
   sw_3 = 0;
   sw_4 = 0;
   left_cir_cnt = 0;
-}
+  view_flag = 0;
+  active_time = 0;
+};
 
 // ビュー消去用関数
-function closeView(opt) {
+var closeView =  function(opt) {
   // 同期処理用変数
   var defer = $.Deferred();
   if (sw_1 == 1) {
@@ -314,15 +414,19 @@ function closeView(opt) {
       // タブビュー消去
       gBrowser.removeTab(gBrowser.tabContainer.childNodes[gBrowser.tabContainer.childNodes.length-1]);
       defer.resolve();
+      // 初期化処処理
+      initTabview();
     }, 500);
   } else if (sw_3 == 1) {
     setTimeout(function() {
       // ブックマークビュー消去
       gBrowser.removeTab(gBrowser.tabContainer.childNodes[gBrowser.tabContainer.childNodes.length-1]);
       // 押されたブックマークブロックのブックマークを開く
-      Block.startBlock(opt);
-    defer.resolve();
+      Block(opt);
+      defer.resolve();
+      // 初期化処処理
+      initBkview();
     }, 500);
   }
   return defer.promise();
-}
+};
